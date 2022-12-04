3bc43b9d32ece43057e7854d1242d43640914557
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2022-12-04 18:20:27 +0900

    update: Laravel IDE Helper 自動補完ツールの導入

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,17 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Session\TokenMismatchException;
 
-class VerifyCsrfToken extends Middleware
+final class VerifyCsrfToken extends Middleware
 {
     /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array<int, string>
      */
-    protected $except = [
-        //
-    ];
+    protected $except = [];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     * @throws TokenMismatchException
+     */
+    public function handle($request, Closure $next): mixed
+    {
+        if (app()->environment() !== 'testing') {
+            return parent::handle($request, $next);
+        }
+
+        return $next($request);
+    }
 }
